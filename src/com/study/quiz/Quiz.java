2af39f02d1cfcc15f70faba87386b39ec450c31b
Kyungseop Ahn <>
2019-09-04 16:53:42 +0900

    Solve SherlockAndTheValidString

--- src/com/study/quiz/Quiz.java
+++ src/com/study/quiz/Quiz.java
@@ -4,6 +4,10 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public abstract class Quiz {
 
@@ -25,6 +29,18 @@ public abstract class Quiz {
             }
         }
         System.out.println(type + " " + sb.toString());
+    }
+
+    protected static String readLineByLine(String filePath) {
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try (Stream<String> stream = Files.lines(Paths.get(filePath), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> contentBuilder.append(s).append("\n"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return contentBuilder.toString();
     }
 
 }
